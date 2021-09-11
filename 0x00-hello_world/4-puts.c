fd250fd678c0296ea233695f45d0720703886f1b
CodexRodney <rodney002mwanje@gmail.com>
2021-09-11 17:30:28 +0300

    Added a c program that prints a string using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-\*A program that prints a string by use of c language*/
+/*A program that prints a string by use of c language*/
 int main(void)
 {
 	char *s = "Programming is like a multilingual puzzle";
