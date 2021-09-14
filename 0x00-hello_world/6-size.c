7ce037c689be9986f9b7a31963aaebbe0603fe5a
CodexRodney <rodney002mwanje@gmail.com>
2021-09-14 11:57:49 +0300

    fixed errors on 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,15 +2,15 @@
 /**
  * main-Prints the size of various types on the computer it is compiled
  *
- * return:Always return 0
+ * Return:Always return 0
  */
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	printf("Size of a char: %lu byte(s)\n",sizeof(char));
+	printf("Size of an int: %lu byte(s)\n",sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n",sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n",sizeof(float));
 	return (0);
 }
 	
