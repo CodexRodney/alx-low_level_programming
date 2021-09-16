03544a763a52ff4620e8e630e60916a3d9c58d82
CodexRodney <rodney002mwanje@gmail.com>
2021-09-16 12:59:44 +0300

    A program that prints a string followed by a new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,11 @@
  */
 void _puts_recursion(char *s)
 {
-	unsigned int index = 0;
-	if (s[index] == "\0")
+	if (*s)
 	{
-	_putchar("\n");
-	return;
+		_putchar(*s);
+		_puts_recursion(s+1);
 	}
-	_putchar(s[index]);
-	_puts_recursion(s+1);
+	else
+		_putchar('\n');
 }
