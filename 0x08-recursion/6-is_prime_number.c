4ebfc71f2f783020be57495bf8fef71da674ee23
CodexRodney <rodney002mwanje@gmail.com>
2021-09-17 00:49:23 +0300

    A function that prints 1 when a number is prime and  0 when otherwise

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+int divis(int i, int j);
+
+/**
+ * divis - Checks if a number is divisible
+ * @i: The number to be checked
+ * @j: The divisor
+ *
+ * Return: 0 if the number is divisible and 1 when otherwise.
+ */
+int divis(int i, int j)
+{
+	if (i % j == 0)
+		return (0);
+
+	if(j == i / 2)
+		return (1);
+
+	return (divis(i, j + 1));
+}
+
+/**
+ * is_prime_number - Checks if a number is prime.
+ * @n: a variable representing the number to be checked
+ *
+ * Return: 0 when n is prime and 1 when n is otherwise
+ */
+int is_prime_number(int n)
+{
+	int j = 2;
+
+	if (n <=1)
+		return (0);
+
+	if (n >= 2 && n <= 3)
+		return (1);
+	return (divis(n, j));
+}
