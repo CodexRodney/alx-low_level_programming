8549d141db16d87743d60a30697ce5ed62ab7d6d
CodexRodney <rodney002mwanje@gmail.com>
2021-09-16 15:24:50 +0300

    A program that prints the length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,18 +2,17 @@
 /**
  * _strlen_recursion- A function that prints the length of a string
  * @s: A pointer fot the string to be inputted
+ *
+ *@i:  length of string 
+ *Return: Always value of variable i
  */
 int _strlen_recursion(char *s)
 {
-	int number = 0;
-	if (s == "\0")
+	int i = 0;
+	if (*s)
 	{
-		return 0;
-	}else
-	{ 
-		return 1;
-		_putchar( + _strlen_recursion(s + 1));
-	;}
+		i++;
+		i = i + _strlen_recursion(s + 1);
+	}
+	return (i);
 }
-
-
