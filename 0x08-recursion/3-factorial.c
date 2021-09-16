539142990e4e7d2f4f2d6f07ca1d8d002d240925
CodexRodney <rodney002mwanje@gmail.com>
2021-09-16 22:42:12 +0300

    A program that returns the factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *factorial- A function that prints the factorial of a number
+ *@n: An integer representing number whose factorial is to be done
+ *
+ * Return: returns -1 if its an error
+ */
+
+int factorial(int n)
+{
+	if(n<0)
+	{
+		return (-1);
+	}
+	if(n <= 1)
+	{
+		return(1);
+	}
+	else
+	{
+		_putchar(n * factorial(n - 1));
+	};
+}
+
