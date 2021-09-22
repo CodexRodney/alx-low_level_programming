04ee915332f2000f9ec79ff91f22e7f1b265b135
CodexRodney <rodney002mwanje@gmail.com>
2021-09-22 16:28:55 +0300

    Added an updated header file

---
+++ 0x0B-array.c/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _puts_recursion(char *s);
+int _putchar(char c);
+void _print_rev_recursioin(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int is_prime_number(int n);
+int _sqrt_recursion(int n);
+char *create_array(unsigned int size, char c);
+
+#endif
