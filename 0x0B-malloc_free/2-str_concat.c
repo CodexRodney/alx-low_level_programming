7841d909ce9e6fe7566bd4dc00723ee204f41fac
CodexRodney <rodney002mwanje@gmail.com>
2021-09-23 02:05:43 +0300

    Fixed error

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,7 +4,7 @@
 
 /**
  * str_concat- Function that concantenates two strings
- * @s1: String 1 
+ * @s1: String 1
  * @s2: String 2
  *
  * Return: a pointer containing s1+s2
@@ -16,7 +16,7 @@ char *str_concat(char *s1, char *s2)
 	int i;
 	int j = 0;
 	int k = 0;
-	
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
@@ -24,7 +24,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s1[i] || s2[i]; i++)
 		j++;
 
-	s3 = malloc(sizeof(char) *j);
+	s3 = malloc(sizeof(char) * j);
 
 	if (s3 == NULL)
 		return (NULL);
