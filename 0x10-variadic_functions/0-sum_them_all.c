5836197819a513ef74ebea9b66ca1f1b37c86b57
CodexRodney <rodney002mwanje@gmail.com>
2021-10-02 00:31:36 +0300

    A function that prints total sum of parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,16 +12,18 @@ int sum_them_all(const unsigned int n, ...)
 {
 	int sum;
 	int i;
+	int a;
 	va_list ap;
 
 	sum = 0;
-	va_start(ap,n);
+	va_start(ap, n);
 
 	if (n == 0)
 		return (0);
 	for (i = 0; i < n; i++)
 	{
-		sum = sum + va_arg(ap, int);
+		a = va_arg(ap, int);
+		sum = sum + a;
 	}
 	va_end(ap);
 	return (sum);
