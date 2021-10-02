1d7800baa8dcb523eae861aaa4fc88a9e6d3aeb7
CodexRodney <rodney002mwanje@gmail.com>
2021-10-02 16:20:55 +0300

    Checks if lowercase:"

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower- Checks for lowercase characters
+ *
+ * Return: 1 if lower
+ *	0 if otherwise
+ */
+
+int _islower(int c)
+{
+	if ( c >= a && c <= z)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
