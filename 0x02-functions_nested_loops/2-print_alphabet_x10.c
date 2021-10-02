0581fb8ffa1b21e5a03d33d976fcd8a9d62cf39b
CodexRodney <rodney002mwanje@gmail.com>
2021-10-02 15:49:16 +0300

    A function that prints alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet- Printys alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	int i = 0;
+	char s = 'a';
+	char nline = '\n';
+
+	while (i < 10)
+	{
+	while (s >= 'a' && s <= 'z')
+	{
+		_putchar(s);
+		s = s + 1;
+	}
+	_putchar(nline);
+	i++;
+}
