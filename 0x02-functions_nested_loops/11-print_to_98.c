a945ed0c0408ca8704f242a00968902baf4069cd
CodexRodney <rodney002mwanje@gmail.com>
2021-10-04 23:37:03 +0300

    A function that computes natural numbers between 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_to_98- prints natural numbers from
+ *	somewhere to 98
+ * @n: number to start from
+ *
+ * Return: Always 0
+ */
+
+void print_to_98(int n)
+{
+	int cpn = n;
+
+	if (cpn < 98)
+	{
+		while ( cpn >= n && cpn <= 98)
+		{
+			if (cpn > n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			cpn++;
+			_putchar('0' + cpn);
+		}
+	}
+	else
+	{
+		while ( cpn <= n && cpn >= 98)
+		{
+			if (cpn < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			cpn--;
+			_putchar('0' + cpn);
+		}
+	}
