92effdd1ce137645497df70fd5c4fffea172fdc1
CodexRodney <rodney002mwanje@gmail.com>
2021-10-04 23:06:51 +0300

    A function that add two intergers and returns result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <main.h>
+
+/**
+ * add- Adds two intergers
+ * num1: First interger
+ * num2: Second interger
+ *
+ * Return: Returns the result of the sum
+ */
+
+int add(int num1, int num2)
+{
+	return (num1+num2);
+}
