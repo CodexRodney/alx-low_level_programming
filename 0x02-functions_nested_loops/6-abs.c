26b1a0aafbc65c72a908af719163d8563b22f7f6
CodexRodney <rodney002mwanje@gmail.com>
2021-10-04 05:18:37 +0300

    A function that computes absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs- Computes the absolute value of a number
+ * 
+ * Return: Always absolute value of a number
+ */
+
+int _abs(int)
+{
+	int a = _abs();
+
+	if (a < 0)
+		return (-a);
+	else
+		return (a);
+}
