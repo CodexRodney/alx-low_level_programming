136af9d63ded672ee97d46fec7292cd3a4f37c19
CodexRodney <rodney002mwanje@gmail.com>
2021-10-08 18:51:04 +0300

    A function that prints a strung before main is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-void myStartFunc (void) __attribute__ (constructor));
+void myStartFunc (void) __attribute__ ((constructor));
 /**
  * myStartFunc- Prints a string before main function is executed
  *
  */
 void myStartFunc (void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
