e95fac345a1c7c945d80e11828ac6c2c157f4b7b
CodexRodney <rodney002mwanje@gmail.com>
2021-10-08 12:48:49 +0300

    Fixed error

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,7 +15,7 @@ size_t print_list(const list_t *h)
 {
 	size_t num = 0;
 
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,6 +5,7 @@
 /**
  * list_len- Returns the no of 
  * elements in a linked list
+ * @h: node to linked list
  *
  * Return: The number of elements
  */
@@ -13,7 +14,7 @@ size_t list_len(const list_t *h)
 {
 	size_t num = 0;
 
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		h = h->next;
 		num++;
