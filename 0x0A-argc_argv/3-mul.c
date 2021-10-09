f74a8192023668096e7f7934b028e02975e4107d
CodexRodney <rodney002mwanje@gmail.com>
2021-10-10 00:57:43 +0300

    A program that multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- A program that multiplies 2 numbers
+ * @argc: The number of arguments passed
+ * @argv: An array of string pointers containing
+ *	pointers to the arguments passed
+ * Return: 1 if arguments passed incorrect
+ *	otherwise 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int firstnum, secondnum, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	firstnum = argv[1];
+	secondnum = argv[2];
+	result = firstnum * secondnum;
+	printf("%d\n", result);
+	return (0);
+}
