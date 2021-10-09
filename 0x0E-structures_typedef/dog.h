8ed512f8b8b30e88852cfd9c7f3a12c7ed5be29f
CodexRodney <rodney002mwanje@gmail.com>
2021-10-09 12:12:17 +0300

    Added Header file with struct data type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef
+#define DOG_H
+
+#include <stdlib.h>
+
+/**
+ * struct dog- Has dog details
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: name of owner of the dog
+ *
+ * Description- Contains Dogs Details
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
