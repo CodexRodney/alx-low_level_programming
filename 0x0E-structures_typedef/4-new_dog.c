eec1c552143a4033cf3d3a63ccdc7af82ed4c7d3
CodexRodney <rodney002mwanje@gmail.com>
2021-10-09 14:52:03 +0300

    A function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,6 +7,7 @@
  * @owner: Owner of dog created
  *
  * Return: NULL if functions fails
+ *	always 0
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
@@ -18,12 +19,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog_new == NULL)
 		return (NULL);
 	if (name == NULL)
-		dog_new.name = " ";
+		dog_new->name = " ";
 	else
-		dog_new.name = name;
+		dog_new->name = name;
 	if (owner == NULL)
-		dog_new.owner = " ";
+		dog_new->owner = " ";
 	else
-		dog_new.owner = owner;
-	dog_new.owner = age;
+		dog_new->owner = owner;
+	dog_new->age = age;
+	return (0);
 }
