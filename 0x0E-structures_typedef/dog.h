fde937c0e41200c829ea342cdc6e68526637e013
CodexRodney <rodney002mwanje@gmail.com>
2021-10-09 14:03:01 +0300

    Updated Header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,22 +19,6 @@ struct dog
 	char *owner;
 };
 
-/**
- * struct dog_t- Contains details of a dog
- * @name: Name of dog
- * @age: Age of dog
- * @owner: Owner of dog
- *
- * Description- Contains Dogs Details
- */
-
-typedef struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
