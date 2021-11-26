cb36589667275a9fb66066f17f6ba7720a7ef133
CodexRodney <rodney002mwanje@gmail.com>
2021-11-26 03:02:07 +0300

    A function that gets nth node of a function

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index- Gets a node at index given
+ * @head: The head node of DLL
+ * @index: The position of node to be gotten
+ *
+ * Return: the nth node
+ *	else NULL if doesn't exist
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *tmp;
+
+	for(k = 0; k < index; k++)
+	{
+		if (head->next == NULL)
+			return (NULL);
+		head = head->next;
+	}
+	tmp = head;
+	return (tmp);
+}
