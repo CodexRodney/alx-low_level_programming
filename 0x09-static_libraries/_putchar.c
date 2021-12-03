5454e29067f485199bf3e6792054b1fe485abd4a
CodexRodney <rodney002mwanje@gmail.com>
2021-12-03 16:12:55 +0300

    Fixed error

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,13 +1,12 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _putchar- Write a single character to the stdout
+ * @c: characters to be written
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Number of characters written
  */
+
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
 }
